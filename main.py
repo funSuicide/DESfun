4bd2e53a4c3a6a117a78abfa87acbba69d6d47eb
Admin <Admin@LAPTOP-U4DN8FG5>
2022-07-09 17:59:05 +0400

    Добавлены функции для работы с ключом (нуждаются в доработке).

--- main.py
+++ main.py
@@ -10,6 +10,20 @@ replace_table = (58, 50, 42, 34, 26, 18, 10, 2,
                  61, 53, 45, 37, 29, 21, 13, 5,
                  63, 55, 47, 39, 31, 23, 15, 7)
 
+invertation_replace_table = (40, 8, 48, 16, 56, 24, 64, 32,
+                             39, 7, 47, 15, 55, 23, 63, 31,
+                             38, 6, 46, 14, 54, 22, 62, 30,
+                             37, 5, 45, 13, 53, 21, 61, 29,
+                             36, 4, 44, 12, 52, 20, 60, 28,
+                             35, 3, 43, 11, 51, 19, 59, 27,
+                             34, 2, 42, 10, 50, 18, 58, 26,
+                             33, 1, 41, 9, 49, 17, 57, 25)
+
+key_replace_table = (57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18,
+                     10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36,
+                     63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22,
+                     14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4)
+
 extend_block_table = (32, 1, 2, 3, 4, 5,
                       4, 5, 6, 7, 8, 9,
                       8, 9, 10, 11, 12, 13,
@@ -18,6 +32,11 @@ extend_block_table = (32, 1, 2, 3, 4, 5,
                       20, 21, 22, 23, 24, 25,
                       24, 25, 26, 27, 28, 29,
                       28, 29, 30, 31, 32, 1)
+
+key_select_table = (14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10,
+                    23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2,
+                    41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48,
+                    44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32)
 
 s_table = [[[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
             [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
@@ -65,7 +84,7 @@ p_table = (16, 7, 20, 21, 29, 12, 28, 17,
            19, 13, 30, 6, 22, 11, 4, 25)
 
 
-def binary_encode(s: str) -> str:
+def binary_encode(s: str) -> str:  # готово
     result = ''
     for element in s:
         tmp = bin(ord(element))[2:]
@@ -74,7 +93,7 @@ def binary_encode(s: str) -> str:
     return result
 
 
-def get_binary_block(text: str) -> list:
+def get_binary_block(text: str) -> list:  # готово
     result = []
     tmp = binary_encode(text)
     while len(tmp) % 64 != 0:
@@ -87,18 +106,26 @@ def get_binary_block(text: str) -> list:
 def p_expend(str: str) -> str:
     result = ''
     for i in extend_block_table:
-        result += str[p_table[i]]
+        result += str[i - 1]
     return result
 
 
-def replace_block(block: str) -> str:
+def replace_block(block: str) -> str:  # готово
     result = ''
     for i in replace_table:
-        result += block[replace_table[i]]
+        print(i-1)
+        result += block[i - 1]
     return result
 
 
-def not_or(str1: str, str2: str) -> str:
+def end_replace_block(block: str) -> str:
+    result = ''
+    for i in invertation_replace_table:
+        result += block[invertation_replace_table[i]]
+    return result
+
+
+def not_or(str1: str, str2: str) -> str:  # готово
     result = ''
     size = len(str1) if len(str1) < len(str2) else len(str2)
     for i in range(size):
@@ -109,16 +136,40 @@ def not_or(str1: str, str2: str) -> str:
 def extend_block(block: str) -> str:
     result = ''
     for i in extend_block_table:
-        result += block[extend_block_table[i]]
+        result += block[i - 1]
     return result
 
 
-def s_expend(str: str, index: int) -> str:
+def s_extend(str: str, index: int) -> str:
     result = ''
     tmp_1 = int((str[0] + str[5]), 2)
     tmp_2 = int((str[1:4]), 2)
     tmp = s_table[index][tmp_1][tmp_2]
     return bin(tmp)
+
+
+def process_key(key: str) -> str:
+    result = ''
+    for i in key_replace_table:
+        result += key[i - 1]
+    return result
+
+
+def spin_key(key: str):
+    tmp_key = process_key(key)
+    first, second = tmp_key[0: 28], tmp_key[28: 56]
+    spin_table = (1, 2, 4, 6, 8, 10, 12, 14, 15, 17, 19, 21, 23, 25, 27, 28)
+    for i in range(1, 17):
+        first_after_spin = first[spin_table[i - 1]:] + first[:spin_table[i - 1]]
+        second_after_spin = second[spin_table[i - 1]:] + second[:spin_table[i - 1]]
+        yield first_after_spin + second_after_spin
+
+
+def key_selection_replacement(key: str) -> list:
+    child_keys = []
+    for child_key56 in spin_key(key):
+        child_keys.append(replace_block(child_key56, key_select_table))
+    return child_keys
 
 
 def s_box_compression(right: str) -> str:
@@ -127,7 +178,7 @@ def s_box_compression(right: str) -> str:
     for i in range(0, len(right), 6):
         result.append(right[i:i + 6])
     for j in range(8):
-        result_2 += s_expend(result[j], j)
+        result_2 += s_extend(result[j], j)
     return p_expend(result_2)
 
 
@@ -138,10 +189,11 @@ def function(right: str, key_i: str) -> str:
     return res_sbox
 
 
-def round_process(block: str, key: str) -> str:
+def encryption_cycle(block: str, key: str) -> str:
+    new_key = process_key(key)
+    child_keys = key_selection_replacement(new_key)
     for i in range(16):
-        # генерация ключа
-        new_key = '1'
+        new_key = child_keys[i]
         left = block[0:32]
         right = block[32:64]
         new_left = right
@@ -149,3 +201,22 @@ def round_process(block: str, key: str) -> str:
         right = not_or(left, res_func)
         block = new_left + right
     return block[32:] + block[:32]
+
+
+def encrypt(data: str, key: str) -> str:
+    result = ''
+    blocks = get_binary_block(data)
+    bin_key = binary_encode(key)
+    print(bin_key)
+    print(len(bin_key))
+    for i in blocks:
+        rep_block = replace_block(i)
+        c_block = encryption_cycle(rep_block, bin_key)
+        end_rep_block = end_replace_block(c_block)
+        result += end_rep_block
+    return result
+
+
+
+
+print(encrypt('Hello', 'Zanderrr'))
